756e9bd526682cc260c19a753d2b998a9bd67691
HiraokaTakuya <hiraoka64@gmail.com>
2017-12-10 14:27:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+For Amazon EC2 (C4 or C5 or M4 or M5) users.
+I use S3 server eu-west-1 region.
+I recommend using the same region and using Ubuntu 16.04 for EC2 users.
+
+USAGE
+
+$ git clone https://github.com/HiraokaTakuya/apery-machine-learning.git
+
+$ cd apery-machine-learning
+
+$ nohup bash install_and_compute_and_send.bash &
+
+Thanks for a gist https://gist.github.com/ohga/ea7db4c85d2fc0370b432acfef426a2c
---
+++ install_and_compute_and_send.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+REGION=eu-west-1
+BUCKETNAME=apery-machine-learning-v1.24.0
+
+TEACHERNODES=10000000
+OUTPUTFILENAME=out_`od -vAn -N8 -tu8 < /dev/urandom | tr -d "[:space:]"`.hcpe
+SHUFOUTPUTFILENAME=shuf${OUTPUTFILENAME}
+
+ROOTS=../../apery-machine-learning-resources/roots.hcp
+
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install g++ make git unzip python-setuptools python2.7-dev -y
+sudo apt-get install python-pip -y
+sudo pip install awscli
+
+git submodule init
+git submodule update --depth=1
+
+git clone -b develop/machine-learning https://github.com/HiraokaTakuya/apery.git
+
+cd apery
+git submodule init
+git submodule update --depth=1
+(cd src && make bmi2 -j && mv apery ../bin)
+(cd utils/shuffle_hcpe && make && mv shuffle_hcpe ../../bin)
+cd bin
+
+if [ $(md5sum ../../apery-machine-learning-resources/roots.hcp | cut -d " " -f 1) = "8d052340bbbf518d05d27f26fd2861a1" ]; then
+    :
+else
+    exit
+fi
+
+if [ $(md5sum 20171106/KPP.bin | cut -d " " -f 1) = "2e0481cc75401eafeb7379ef514272e1" ]; then
+    :
+else
+    exit
+fi
+
+if [ $(md5sum 20171106/KKP.bin | cut -d " " -f 1) = "26f545b7dd09b317483deb3cd4f9052f" ]; then
+    :
+else
+    exit
+fi
+
+while :
+do
+    ./apery make_teacher $ROOTS $OUTPUTFILENAME $(nproc) $TEACHERNODES
+    ./shuffle_hcpe $OUTPUTFILENAME $SHUFOUTPUTFILENAME && rm $OUTPUTFILENAME && aws --region eu-west-1 s3 cp $SHUFOUTPUTFILENAME s3://apery-machine-learning-v1.24.0/data/ --no-sign-request && rm $SHUFOUTPUTFILENAME
+done
